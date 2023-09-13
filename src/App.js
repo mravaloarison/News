1b136b7d2fd3eb3ee4dfd5abecafdf1bcacc40e5
mami-em <maminiainaravaloarison@gmail.com>
2023-09-12 21:22:23 -0400

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+import NewsComponent from "./NewsComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="lg:px-8 lg:py-5 p-4 bg-blue-600 text-slate-50">
+        <div className="flex justify-between">
+          <div className="flex justify-center items-center gap-2">
+            <div className="w-7 h-7 border"></div>
+            <p className="text-2xl font-light">Latest News</p>
+          </div>
+          <a href="/" className="text-gray-300 flex gap-2 items-center">
+            <p>Github</p>
+            <ArrowTopRightOnSquareIcon className="w-6 h-6" />
+          </a>
+        </div>
       </header>
+      <NewsComponent />
+      <footer>
+        <div>
+
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/NewsComponent.js
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+function NewsComponent() {
+  const [articles, setArticles] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('general');
+
+  const categories = [
+    { name: "general", emoji: "🤷‍♂️" },
+    { name: "business", emoji: "🤝" },
+    { name: "entertainment", emoji: "🎬" },
+    { name: "health", emoji: "🩺" },
+    { name: "science", emoji: "🧬" },
+    { name: "sports", emoji: "🏅" },
+    { name: "technology", emoji: "👨‍💻" }
+  ];
+  
+  useEffect(() => {
+    const baseUrl = 'https://newsapi.org/v2/top-headlines?';
+    const apiKey = process.env.REACT_APP_API_KEY_3;
+
+    const url = `${baseUrl}${selectedCategory !== 'general' ? `category=${selectedCategory}&` : ''}country=us&apiKey=${apiKey}`;
+
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === 'ok') {
+                setArticles(data.articles); 
+            } else {
+                console.error('Error: Data status is not ok');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }, [selectedCategory]);
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+
+        const apiKey = process.env.REACT_APP_API_KEY_3;
+    
+        const searchUrl = `https://newsapi.org/v2/everything?q=${searchInput}&sortBy=popularity&apiKey=${apiKey}`;
+    
+        fetch(searchUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.status === 'ok') {
+              setArticles(data.articles);
+            } else {
+              console.error('Error: Data status is not ok');
+            }
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
+
+    return (
+        <div>
+            <div className="flex flex-col justify-center items-center lg:py-10 lg:gap-8 gap-4">
+
+                <h1 className="text-[3rem] font-thin text-gray-700 px-4 pt-6">What's on the news?</h1>
+                <div className="px-4 w-full max-w-lg mx-auto">
+                    <form onSubmit={handleSearchSubmit} className="w-full border border-gray-300 rounded py-2.5 px-4 flex justify-between">
+                        <input
+                            className="lg:text-xl text-lg w-full outline-none text-gray-600"
+                            type="text"
+                            placeholder="Search by keyword"
+                            value={searchInput}
+                            onChange={(e) => setSearchInput(e.target.value)}
+                        />
+                        <button type="submit" className="">
+                            <MagnifyingGlassIcon className="w-5 h-5 text-gray-400 stroke-2" />
+                        </button>
+                    </form>
+                </div>
+
+                <div className="max-w-6xl mx-auto w-full">
+                    <ul className="flex items-center lg:justify-center overflow-scroll p-4 lg:gap-4 gap-2 text-blue-600">
+                        {categories.map((item, index) => (
+                            <li key={index} className="group">
+                                <button
+                                onClick={() => handleCategoryClick(item.name)}
+                                className={`border ${selectedCategory === item.name ? 'text-white bg-blue-600' : 'border-blue-200'} px-5 py-2 flex items-center gap-2 rounded-full text-sm font-semibold group-hover:border-blue-400 capitalize`}
+                                >
+                                    <span className="text-lg">{item.emoji} </span> {item.name}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+            </div>
+            <ul className="grid lg:grid-cols-3 md:grid-cols-2 p-4 gap-10 md:gap-8 w-full max-w-6xl mx-auto">
+            {articles.map((article, index) => (
+                <li key={index} className="">
+                    <a className="group" href={article.url} target="_blank" rel="noopener noreferrer">
+                        <img className="w-full h-52 shadow-md bg-cover bg-center" src={article.urlToImage} alt="pic" />
+                        <h2 className="font-semibold pt-2.5">{article.title}</h2>
+                        {/* <p>{article.description}</p> */}
+                        <div className="text-gray-400 text-sm flex gap-2 divide-x pt-1.5">
+                            <p className="font-semibold">{article.source.name}</p>
+                            <p className="pl-2">By {article.author}</p>
+                        </div>
+                    </a>
+                </li>
+            ))}
+            </ul>
+        </div>
+    );
+}
+
+export default NewsComponent;
