49f7c96d86e65556565f7b0244eb8d1f63d9f1f6
mami-em <maminiainaravaloarison@gmail.com>
2023-09-13 11:45:12 -0400

    link in header edited

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ function App() {
     <div>
       <header className="lg:px-8 lg:py-5 p-4 bg-blue-600 text-slate-50">
         <div className="flex justify-between">
-          <div className="flex justify-center items-center gap-2">
-            <div className="w-7 h-7 border"></div>
-            <p className="text-2xl font-light">Latest News</p>
-          </div>
-          <a href="/" className="text-gray-300 flex gap-2 items-center">
+          <a href="/" className="flex justify-center items-center text-xl font-semibold">
+            <span className="px-2.5 py-1.5 border border-white rounded-bl">Latest</span>
+            <span className="px-2.5 py-1.5 border border-white bg-white text-blue-600 rounded-tr">News</span>
+          </a>
+          <a href="https://github.com/mravaloarison/News" target="_blank" rel="noopener noreferrer" className="text-gray-300 flex gap-2 items-center">
             <p>Github</p>
             <ArrowTopRightOnSquareIcon className="w-6 h-6" />
           </a>
         </div>
       </header>
       <NewsComponent />
-      <footer>
-        <div>
-
+      <footer className="w-full flex justify-center items-center">
+        <div className="p-8 text-xs text-gray-400">
+          © Latest News 2023, by Mami.
         </div>
       </footer>
     </div>
