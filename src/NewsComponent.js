558fe8424f4a76961f2c0e84585d97439293e7f9
mami-em <maminiainaravaloarison@gmail.com>
2023-09-13 13:18:12 -0400

    data status updated

--- src/NewsComponent.js
+++ src/NewsComponent.js
@@ -29,7 +29,7 @@ function NewsComponent() {
             if (data.status === 'ok') {
                 setArticles(data.articles); 
             } else {
-                console.error('Error: Data status is not ok');
+                console.error(data.status);
             }
         })
         .catch((error) => {
